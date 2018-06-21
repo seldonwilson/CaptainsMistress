16deab819844243b5c98ada7df488b5766071aa5
Daniel Wilson <seldon.wilson@gmail.com>
2018-06-21 13:59:33 -0700

    Added InputString.cpp
    Provides implementation of isInt() and a stub for isIntInRange.

---
+++ InputString.cpp
@@ -0,0 +1,24 @@
+#include "InputString.hpp"
+
+using std::string;
+
+InputString::InputString(string _input) {
+   input = _input;
+}
+
+bool InputString::isInt() const {
+   try {
+       std::stoi(input);
+       return true;
+   } catch (...) {
+      return false;
+   }
+}
+
+bool InputString::isIntInRange(int low, int high) const {
+   if (!isInt()) {
+      return false;
+   } else {
+      return true;
+   }
+}
