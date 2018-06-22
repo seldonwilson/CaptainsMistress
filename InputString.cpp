ce2046bed7b49f218d4e79987f0aa2141ba57e07
Daniel Wilson <seldon.wilson@gmail.com>
2018-06-22 08:42:59 -0700

    isInt() bug fix
    isInt() no longer will return true on strings that begin with a valid integer, then later contain invalid characters.

--- InputString.cpp
+++ InputString.cpp
@@ -2,6 +2,7 @@
 
 using std::string;
 using std::stoi;
+using std::to_string;
 
 InputString::InputString(string _input) {
    input = _input;
@@ -10,8 +11,14 @@ InputString::InputString(string _input) {
    // Returns true if input can represent a value in the range of an int
 bool InputString::isInt() const {
    try {
-       stoi(input);
-       return true;
+       int num = stoi(input);
+
+          // If # of digits in num == input.length(), true. Else, false.
+       if (to_string(num).length() == input.length()) {
+          return true;
+       } else {
+          return false;
+       }
    } catch (...) {
       return false;
    }
