3df0a096c59da464a800a8463455d3fef1b6194a
David Coopersmith <dcoopersmith@mvusd.net>
2018-06-23 11:39:45 -0700

    somehow got GameLogic into master :(

---
+++ GameLogic.hpp
@@ -0,0 +1,19 @@
+/************************************************************************************
+filename: GameLogic.hpp
+ authors: Coop & Danny
+************************************************************************************/
+
+#ifndef GAME_LOGIC_HPP_
+#define GAME_LOGIC_HPP_
+
+#include "Board.hpp"
+
+namespace GameLogic {
+
+      // did a given move win the game?
+   bool isWin(const Board & brd, int row, int col);
+   bool isColFull(const Board & brd, int col);
+   void placeToken(Board & brd, cellState currToken, int col);
+}
+
+#endif // GAME_LOGIC_HPP_
